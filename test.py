0290fae44cf45b059335206a2dd19f55782585f0
HA-yeoungwoo <tipia0036@gmail.com>
2024-01-22 18:15:25 +0900

    ha

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 def add(a, b):
     
+    return a+b
+    
 def sub(a, b):
 
 def mul(a, b):
     
     
 def main():
-
-
+    a=int(input('a='))
+    print(a)
+    b=int(input('b='))
+    print('b')
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
 
 if __name__ == "__main__":
     main()
